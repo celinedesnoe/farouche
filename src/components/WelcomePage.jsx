8a0e47b500f1eacfa6cb9f59c6e1c9a59ace8ed5
Celine <celine.desnoe@weloop.io>
2020-07-19 16:52:49 +0200

    add link for social networks

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -6,8 +6,12 @@ import CountDown from "./CountDown";
 
 const WelcomePage = () => {
   const socialNetworks = [
-    { type: "facebook", link: "" },
-    { type: "instagram", link: "" },
+    {
+      type: "facebook",
+      link:
+        "https://m.facebook.com/Farouche-Éditions-100211638446972?__nodl&refsrc=https%3A%2F%2Fm.facebook.com%2Ffbrdr%2F274%2F100211638446972%2F&_rdr",
+    },
+    { type: "instagram", link: "https://www.instagram.com/farouche.editions/" },
     { type: "twitter", link: "" },
   ];
 
@@ -33,13 +37,14 @@ const WelcomePage = () => {
       <div className="list-networks">
         {socialNetworks.map((oneNetwork) => {
           return (
-            <div
+            <a
               className="one-network"
               href={oneNetwork.link}
               key={oneNetwork.type}
+              target="_blank"
             >
               <FontAwesomeIcon icon={["fab", oneNetwork.type]} />
-            </div>
+            </a>
           );
         })}
       </div>
